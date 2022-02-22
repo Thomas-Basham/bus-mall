d4e80ba4adff722709655b010eb565071e9dba2d
Thomas Basham <Bashamtg@gmail.com>
2022-02-22 15:18:10 -0800

    rendered chart to replace list

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // console.log('oh, hello');
 
 // total rounds the user will be allowed to vote. Needs to be 25. changed to 5 for testing purposes
-let roundsAllowed = 55;
+let roundsAllowed =25;
 
 
 // empty array to push product obejcts to
@@ -15,8 +15,11 @@ let imgOne = document.getElementById('img-one');
 let imgTwo = document.getElementById('img-two');
 let imgTre = document.getElementById('img-tre');
 // console.log(imgOne);
-let resultsBtn = document.getElementById('show-results-btn');
-let showResults = document.getElementById('display-results-list');
+// let resultsBtn = document.getElementById('show-results-btn');
+// let showResults = document.getElementById('display-results-list');
+let ctx = document.getElementById('myChart').getContext('2d');
+
+
 
 // constructor function for new product info pushing to allProducts[]
 function Product(name, fileExtension = 'jpg'){
@@ -51,36 +54,29 @@ new Product('wine-glass');
 
 
 
+
 // function gets a random product
 function getRandomProduct() {
   return Math.floor(Math.random()* allProducts.length);
 }
 
+let randomProducts = [];
+// hint for 2 unique rounds: 6 numbers for 2 unique rounds.
+
 
 // render images
 function renderImgs(){
-  let randomProduct1 = getRandomProduct();
-  let randomProduct2 = getRandomProduct();
-  let randomProduct3 = getRandomProduct();
-
-  // need the validation to make sure they do not show up in the same round
-  // NOTE: Your lab will require you to have 3 unique images per round
-  // HINT: Consider using a container to store your random indexes and then validate if there are 3 unique numbers in that container
-
-  while(randomProduct1 === randomProduct2){
-    randomProduct2 = getRandomProduct();
+  while(randomProducts.length < 3){
+    let randoNum = getRandomProduct();
+    while(!randomProducts.includes(randoNum)){
+      randomProducts.push(randoNum);
+    }
   }
 
-  while(randomProduct1 === randomProduct3){
-    randomProduct3 = getRandomProduct();
-  }
-  while(randomProduct2 === randomProduct3){
-    randomProduct2 = getRandomProduct();
-  }
+  let randomProduct1 = randomProducts.pop();
+  let randomProduct2 = randomProducts.pop();
+  let randomProduct3 = randomProducts.pop();
 
-  // while(randomProduct2 === randomProduct1 || randomProduct3){
-  //   randomProduct3 = getRandomProduct();
-  // }
 
   imgOne.src = allProducts[randomProduct1].src;
   imgOne.alt = allProducts[randomProduct1].name;
@@ -94,8 +90,8 @@ function renderImgs(){
   imgTre.alt = allProducts[randomProduct3].name;
   allProducts[randomProduct3].views++;
 }
-
 renderImgs();
+
 console.log(allProducts);
 
 console.log(roundsAllowed);
@@ -119,34 +115,90 @@ function handleClick(event){
   // once voting rounds completed -  stop clicks
   if(roundsAllowed === 0){
     imgContainer.removeEventListener('click', handleClick);
+    renderChart();
   }
 }
 
-function handleShowResults(event){  //eslint-disable-line
-  // if no more votes - then render a list
+// function handleShowResults(event){  //eslint-disable-line
+//   // if no more votes - then render a list
 
-  if(roundsAllowed>0){
-    alert('Please complete 25 rounds of voting, then click the Show Results button to see your results');
+//   if(roundsAllowed>0){
+//     alert('Please complete 25 rounds of voting, then click the Show Results button to see your results');
+//   }
+
+//   if(roundsAllowed === 0){
+//     for(let i = 0; i < allProducts.length; i++){
+//       let li = document.createElement('li');
+//       li.textContent = `${allProducts[i].name} was viewed ${allProducts[i].views} times, and was voted for ${allProducts[i].clicks} times.`;
+//       showResults.appendChild(li);
+//     }
+//   }
+// }
+
+
+
+function renderChart(){
+
+  let productNames = [];
+
+  let productClicks = [];
+  let productViews = [];
+
+  for(let i = 0; i < allProducts.length; i++){
+    productNames.push(allProducts[i].name);
+    productClicks.push(allProducts[i].clicks);
+    productViews.push(allProducts[i].views);
   }
 
-  if(roundsAllowed === 0){
-    for(let i = 0; i < allProducts.length; i++){
-      let li = document.createElement('li');
-      li.textContent = `${allProducts[i].name} was viewed ${allProducts[i].views} times, and was voted for ${allProducts[i].clicks} times.`;
-      showResults.appendChild(li);
+
+  let chartObject = { //eslint-disable-line
+    type: 'bar',
+    data: {
+      labels: productNames,
+      datasets: [{
+        label: '# of Votes',
+        data: productClicks,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+        ],
+        borderWidth: 1,
+        hoverBorderColor: 'black'
+      },
+      {
+        label: '# of Views',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.2)',
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+        ],
+        borderWidth: 1,
+        hoverBorderColor: 'black'
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
     }
-  }
-}
+  };
 
+
+  const myChart = new Chart(ctx, chartObject);
+}
 
 // what event we listen to
 
+
 imgContainer.addEventListener('click', handleClick);
 
-resultsBtn.addEventListener('click', handleShowResults);
-
-
-
+// resultsBtn.addEventListener('click', handleShowResults);
 
 
 
